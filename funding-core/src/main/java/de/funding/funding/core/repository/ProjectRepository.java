77fc494096fc809b0368fde9c1ab017c75187aa7
Dominik Grodt <sanitysdownfall@gmail.com>
2018-10-05 22:26:20 +0200

    projectrepository

--- funding-core/src/main/java/de/funding/funding/core/repository/ProjectRepository.java
+++ funding-core/src/main/java/de/funding/funding/core/repository/ProjectRepository.java
@@ -2,9 +2,21 @@ package de.funding.funding.core.repository;
 
 import de.funding.funding.entity.Project;
 
+import java.net.URI;
+import java.util.List;
 import java.util.UUID;
 
 public interface ProjectRepository {
 
+  public enum SortBy {CreationDate, ModifiedDate, Trending, Popular}
+
   Project getProject(UUID uuid);
+
+  List<Project> getProjects(final Long offset, final Long limit, final SortBy sortBy,
+                            final Boolean asc);
+
+  URI getTeaserPictureUri(UUID projectId);
+
+  List<Project> searchByTitle(final String title, final Long offset, final Long limit,
+                              final SortBy sortBy, final Boolean asc);
 }
