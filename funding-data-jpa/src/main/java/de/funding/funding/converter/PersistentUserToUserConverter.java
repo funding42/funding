74bccb419abf5e10ef4f8c6ca6db90afcd627141
NicolasKotz <nicolas.kotz@gmx.de>
2018-10-06 10:52:14 +0200

    Converter Null-Safety

--- funding-data-jpa/src/main/java/de/funding/funding/converter/PersistentUserToUserConverter.java
+++ funding-data-jpa/src/main/java/de/funding/funding/converter/PersistentUserToUserConverter.java
@@ -1,27 +1,31 @@
 package de.funding.funding.converter;
 
-import de.funding.funding.entity.PersistentUser;
-import de.funding.funding.entity.Skill;
-import de.funding.funding.entity.User;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import de.funding.funding.entity.PersistentUser;
+import de.funding.funding.entity.Skill;
+import de.funding.funding.entity.User;
 
 @Component
 public class PersistentUserToUserConverter implements Converter<PersistentUser, User> {
 
-  @Autowired
-  private PersistentSkillToSkillConverter persistentSkillToSkillConverter;
+	@Autowired
+	private PersistentSkillToSkillConverter persistentSkillToSkillConverter;
 
-  @Override
-  public User convert(final PersistentUser source) {
-    final List<Skill> skills =
-        source.getSkills().stream().map(persistentSkillToSkillConverter::convert)
-            .collect(Collectors.toList());
-    return new User(source.getId(), source.getFirstName(), source.getLastName(), source.getType(),
-        skills, source.getDescription());
-  }
+	@Override
+	public User convert(final PersistentUser source) {
+		List<Skill> skills = null;
+		if (source.getSkills() != null) {
+			skills =
+					source.getSkills().stream().map(persistentSkillToSkillConverter::convert)
+					.collect(Collectors.toList());
+		}
+		return new User(source.getId(), source.getFirstName(), source.getLastName(), source.getType(),
+				skills, source.getDescription());
+	}
 }
--- funding-data-jpa/src/main/java/de/funding/funding/converter/UserToPersistentConverter.java
+++ funding-data-jpa/src/main/java/de/funding/funding/converter/UserToPersistentConverter.java
@@ -1,29 +1,32 @@
 package de.funding.funding.converter;
 
-import de.funding.funding.entity.PersistentUser;
-import de.funding.funding.entity.User;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import java.util.stream.Collectors;
+import de.funding.funding.entity.PersistentUser;
+import de.funding.funding.entity.User;
 
 @Component
 public class UserToPersistentConverter implements Converter<User, PersistentUser> {
 
-  @Autowired
-  private SkillToPersistentSkillConverter skillToPersistentSkillConverter;
+	@Autowired
+	private SkillToPersistentSkillConverter skillToPersistentSkillConverter;
 
-  @Override
-  public PersistentUser convert(final User source) {
-    final PersistentUser target = new PersistentUser();
-    target.setDescription(source.getDescription());
-    target.setFirstName(source.getPrename());
-    target.setLastName(source.getSurname());
-    target.setId(source.getUuid());
-    target.setType(source.getType());
-    target.setSkills(source.getSkills().stream().map(skillToPersistentSkillConverter::convert)
-        .collect(Collectors.toSet()));
-    return target;
-  }
+	@Override
+	public PersistentUser convert(final User source) {
+		final PersistentUser target = new PersistentUser();
+		target.setDescription(source.getDescription());
+		target.setFirstName(source.getPrename());
+		target.setLastName(source.getSurname());
+		target.setId(source.getUuid());
+		target.setType(source.getType());
+		if (source.getSkills() != null) {
+			target.setSkills(source.getSkills().stream().map(skillToPersistentSkillConverter::convert)
+					.collect(Collectors.toSet()));
+		}
+		return target;
+	}
 }
